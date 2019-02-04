cbd2cd30d0b368ddbd1436802c44b7a69b531b3b
Yue (Zed) Yang <shoominga@gmail.com>
2019-02-04 14:20:39 +0800

    add paginator

--- shop/views.py
+++ shop/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework import generics, mixins, viewsets
-from django.http import HttpResponse
 from .models import Category, Product
 from .filter import ProductFilter
+from .paginator import ProductPagination
 from .serializer import CategorySerializer, ProductSerializer
 
+
 # Create your views here.
-class ProductViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,
-                         viewsets.GenericViewSet):
+class ProductViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     """
     Product
     """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
+    pagination_class = ProductPagination
     filter_class = ProductFilter
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
     search_fields = ('name', 'description')
     ordering_fields = ('price', 'created')
 
 
-class CategoryViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,
-                      viewsets.GenericViewSet):
+class CategoryViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     """
     Category
     """
