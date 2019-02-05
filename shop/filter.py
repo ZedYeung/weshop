0ebb55b37f323dbffe0d8227f8a64b891d369a48
Yue (Zed) Yang <shoominga@gmail.com>
2019-02-06 07:50:39 +0800

    add category filter

--- shop/filter.py
+++ shop/filter.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.db.models import Q
 from .models import Product
 
 
@@ -8,6 +9,11 @@ class ProductFilter(django_filters.rest_framework.FilterSet):
     """
     min_price = django_filters.NumberFilter(field_name='price', help_text='minimum price', lookup_expr='gte')
     max_price = django_filters.NumberFilter(field_name='price', help_text='maximum price', lookup_expr='lte')
+    category = django_filters.NumberFilter(method='category_filter')
+
+    def category_filter(self, queryset, name, value):
+        return queryset.filter(Q(category_id=value))
+
 
     class Meta:
         model = Product
