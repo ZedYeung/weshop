c324dab5135f8d2fea2e93df068720d44888df3c
Yue (Zed) Yang <shoominga@gmail.com>
2019-02-06 07:50:53 +0800

    modify paginator page size

--- shop/paginator.py
+++ shop/paginator.py
@@ -2,7 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class ProductPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 8
     page_size_query_param = 'page_size'
     page_query_param = 'page'
     max_page_size = 100
