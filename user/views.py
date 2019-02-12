a543cb3ee3bd60d21384c82bf71051daaa4e4443
Yue (Zed) Yang <shoominga@gmail.com>
2019-02-13 06:41:45 +0800

    feature: auto login after register

--- user/views.py
+++ user/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from rest_framework import mixins, viewsets
 from rest_framework import permissions
 from rest_framework import authentication
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework_jwt.serializers import jwt_payload_handler, jwt_encode_handler
 from .serializers import UserRegisterSerializer, UserProfileSerializer
 from .models import User
 
@@ -38,6 +41,21 @@ class UserViewset(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.Upd
     # return current user with any id
     def get_object(self):
         return self.request.user
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.perform_create(serializer)
+
+        res = serializer.data
+        payload = jwt_payload_handler(user)
+        res["token"] = jwt_encode_handler(payload)
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(res, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        return serializer.save()
 
 
 
