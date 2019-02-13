38247b1770bf41226432a0b876722be9afe61d45
Yue (Zed) Yang <shoominga@gmail.com>
2019-02-14 06:09:23 +0800

    modify ordering productList css

--- src/components/Ordering.js
+++ src/components/Ordering.js
@@ -10,18 +10,12 @@ export class Ordering extends Component {
     render() {
         return (
             <div className="product-ordering">
-                <Radio.Group className="ordering-radio" buttonStyle="outline" onChange={this.onOrderingChange} size="small" >
-                {/* <Radio.Group buttonStyle="outline" onChange={this.onOrderingChange}> */}
-                    <p className="ordering-radio">Price</p>
-                
-                    {/* Price */}
-                    {/* <Radio.Button value="Price" disabled={true}>Price</Radio.Button> */}
-                    <Radio.Button value="price"><Icon type="caret-up" /> </Radio.Button>
+                <Radio.Group buttonStyle="outline" onChange={this.onOrderingChange} size="small" >
+                    Price{' '}
+                    <Radio.Button value="price"><Icon type="caret-up" />  </Radio.Button>
                     <Radio.Button value="-price"><Icon type="caret-down" /></Radio.Button>
-                    {/* <Radio.Button value="Created" disabled={true}>Created</Radio.Button> */}
-                    <p className="ordering-radio">Newest</p>
-                
-                    {/* Newest */}
+
+                    {' '}Newest{' '}
                     <Radio.Button value="created"> <Icon type="caret-up" /></Radio.Button>
                     <Radio.Button value="-created"> <Icon type="caret-down" /></Radio.Button>
                 </Radio.Group>
