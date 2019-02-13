729dd2c0930c8938a85098facf5c992abcd758a0
Yue (Zed) Yang <shoominga@gmail.com>
2019-02-14 07:09:38 +0800

    feature: thankyou page as card

--- src/components/Thankyou.js
+++ src/components/Thankyou.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Card } from 'antd';
 
 export class Thankyou extends Component {
     render() {
         return (
-            <div>
-                Thank you
-            </div>
+            <Card className="thankyou" title="Thanks for your order" bordered={false} >
+                <p>This is just a demo website</p>
+                <p>You would not be charged and would not receive any package</p>
+                <p>You would not be charged and would not receive any package</p>
+                <p>You would not be charged and would not receive any package</p>
+                <p>Best</p>
+          </Card>
         )
     }
 }
