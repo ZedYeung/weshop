4d6cc87319f4f9e8ef89af9354fb4b47f31a5db5
Yue (Zed) Yang <shoominga@gmail.com>
2019-02-16 02:38:24 +0800

    api: process.env

--- src/components/api.js
+++ src/components/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'universal-cookie';
 import { AUTH_PREFIX, TOKEN_KEY } from '../.env';
 
-const local_host = "http://localhost:8000";
+const host = process.env.HOST || "http://localhost:8000";
 
 axios.interceptors.request.use(
     (config) => {
@@ -17,87 +17,87 @@ axios.interceptors.request.use(
 )
 
 export const getProducts = (params) => {
-    return axios.get(`${local_host}/product/`, { params: params });
+    return axios.get(`${host}/product/`, { params: params });
 };
 
 export const getCategories = () => {
     // must add the last /
     // otherwise blocked by cors
-    return axios.get(`${local_host}/category/` );
+    return axios.get(`${host}/category/` );
 };
 
 export const login = (params) => {
-    return axios.post(`${local_host}/login/`, params)
+    return axios.post(`${host}/login/`, params)
 }
 
 export const register = (params) => {
-    return axios.post(`${local_host}/user/`, params)
+    return axios.post(`${host}/user/`, params)
 }
 
 export const getProduct = (productID) => {
-    return axios.get(`${local_host}/product/${productID}/`)
+    return axios.get(`${host}/product/${productID}/`)
 }
 
 export const getUserProfile = () => {
-    return axios.get(`${local_host}/user/1/`)
+    return axios.get(`${host}/user/1/`)
 }
 
 export const updateUserProfile = (params) => {
-    return axios.put(`${local_host}/user/1/`, params )
+    return axios.put(`${host}/user/1/`, params )
 }
 
 export const getAddresses = () => {
-    return axios.get(`${local_host}/address/`)
+    return axios.get(`${host}/address/`)
 }
 
 export const deleteAddress = (addressID) => {
-    return axios.delete(`${local_host}/address/${addressID}/`)
+    return axios.delete(`${host}/address/${addressID}/`)
 }
 
 export const updateAddress = (addressID, params) => {
-    return axios.patch(`${local_host}/address/${addressID}/`, params)
+    return axios.patch(`${host}/address/${addressID}/`, params)
 }
 
 export const createAddress = (params) => {
-    return axios.post(`${local_host}/address/`, params)
+    return axios.post(`${host}/address/`, params)
 }
 
 export const getCart = () => {
-    return axios.get(`${local_host}/cart/`)
+    return axios.get(`${host}/cart/`)
 }
 
 export const addCart = (params) => {
-    return axios.post(`${local_host}/cart/`, params)
+    return axios.post(`${host}/cart/`, params)
 }
 
 export const deleteCart = (productID) => {
-    return axios.delete(`${local_host}/cart/${productID}/`)
+    return axios.delete(`${host}/cart/${productID}/`)
 }
 
 export const updateCart = (productID, params) => {
-    return axios.patch(`${local_host}/cart/${productID}/`, params)
+    return axios.patch(`${host}/cart/${productID}/`, params)
 }
 
 export const getOrders = () => {
-    return axios.get(`${local_host}/order/`)
+    return axios.get(`${host}/order/`)
 }
 
 export const getOrder = (orderID) => {
-    return axios.get(`${local_host}/order/${orderID}/`)
+    return axios.get(`${host}/order/${orderID}/`)
 }
 
 export const deleteOrder = (orderID) => {
-    return axios.delete(`${local_host}/order/${orderID}/`)
+    return axios.delete(`${host}/order/${orderID}/`)
 }
 
 export const createOrder = (params) => {
-    return axios.post(`${local_host}/order/`, params)
+    return axios.post(`${host}/order/`, params)
 }
 
 export const checkout = (params) => {
-    return axios.post(`${local_host}/checkout/`, params)
+    return axios.post(`${host}/checkout/`, params)
 }
 
-export const loginAmazon = `${local_host}/login/amazon/`
-export const loginGoogle = `${local_host}/login/google-oauth2/`
-export const loginTwitter = `${local_host}/login/twitter/`
+export const loginAmazon = `${host}/login/amazon/`
+export const loginGoogle = `${host}/login/google-oauth2/`
+export const loginTwitter = `${host}/login/twitter/`
