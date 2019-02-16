5989551e390d2395372edd7cdf10a131bab85816
Yue (Zed) Yang <shoominga@gmail.com>
2019-02-17 05:32:18 +0800

    deploy: add allow host for django

--- backend/Weshop/settings.py
+++ backend/Weshop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'qs(qih1plsr&9%^mnpl($pixljb^b10j#ew_i$xqe5yg$$jsyo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'backend']
 
 AUTH_USER_MODEL = 'user.User'
 
