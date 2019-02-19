84a170a6cc8b201c60931aa5824042b343470549
Yue (Zed) Yang <shoominga@gmail.com>
2019-02-19 12:36:21 +0800

    terraform: add aws iam

---
+++ terraform/aws_iam.tf
@@ -0,0 +1,84 @@
+resource "aws_iam_role" "ecs_service" {
+  name = "ecs_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "ecs_service" {
+  name = "ecs_policy"
+  role = "${aws_iam_role.ecs_service.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:Describe*",
+        "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+        "elasticloadbalancing:DeregisterTargets",
+        "elasticloadbalancing:Describe*",
+        "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+        "elasticloadbalancing:RegisterTargets"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "ecs_instance" {
+  name = "ecs-instance-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "ecs_instance" {
+  name = "ecs-instance-profile"
+  role = "${aws_iam_role.ecs_instance.name}"
+}
+
+data "template_file" "ecs_instance_profile" {
+  template = "${file("${path.module}/ecs-instance-profile-policy.json")}"
+
+  vars {
+    app_log_group_arn = "${aws_cloudwatch_log_group.app.arn}"
+    ecs_log_group_arn = "${aws_cloudwatch_log_group.ecs.arn}"
+  }
+}
+
+resource "aws_iam_role_policy" "ecs_instance" {
+  name   = "ecs-instance-role"
+  role   = "${aws_iam_role.ecs_instance.name}"
+  policy = "${data.template_file.ecs_instance_profile.rendered}"
+}
