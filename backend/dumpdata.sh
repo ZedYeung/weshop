7508545be07b7cd8364d596d94e6f55b42c27347
Yue (Zed) Yang <shoominga@gmail.com>
2019-02-24 06:31:43 +0800

    pur: update requirements.txt

--- backend/dumpdata.sh
+++ backend/dumpdata.sh
@@ -2,6 +2,7 @@
 
 # generate requirement.txt
 # pipreqs .
+# pur -r requirements.txt
 python manage.py dumpdata --natural-foreign \
    --exclude auth.permission --exclude contenttypes \
    --indent 4 > data.json
