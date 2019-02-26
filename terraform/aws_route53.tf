37663aa4298507ff4ecd85c0f808e3d1280ab464
Yue (Zed) Yang <shoominga@gmail.com>
2019-02-26 12:37:38 +0800

    aws route 53 - isolate backend api subdomain

--- terraform/aws_route53.tf
+++ terraform/aws_route53.tf
@@ -1,15 +1,31 @@
-resource "aws_route53_record" "app" {
+resource "aws_route53_record" "frontend" {
   zone_id = "${var.route53_zone_id}"
-  name    = "${var.app_domain}"
+  name    = "${var.frontend_domain}"
   type    = "A"
 
   alias {
-    name                   = "${aws_alb.alb-ecs-app.dns_name}"
-    zone_id                = "${aws_alb.alb-ecs-app.zone_id}"
+    name                   = "${aws_alb.alb-ecs-frontend.dns_name}"
+    zone_id                = "${aws_alb.alb-ecs-frontend.zone_id}"
     evaluate_target_health = true
   }
 
   depends_on = [
-    "aws_alb.alb-ecs-app",
+    "aws_alb.alb-ecs-frontend",
+  ]
+}
+
+resource "aws_route53_record" "backend" {
+  zone_id = "${var.route53_zone_id}"
+  name    = "${var.backend_domain}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_alb.alb-ecs-backend.dns_name}"
+    zone_id                = "${aws_alb.alb-ecs-backend.zone_id}"
+    evaluate_target_health = true
+  }
+
+  depends_on = [
+    "aws_alb.alb-ecs-backend",
   ]
 }
