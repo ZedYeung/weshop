08a07ef54e8afae6e00aa750c6789b1ecf723102
Yue (Zed) Yang <shoominga@gmail.com>
2019-02-27 02:01:59 +0800

    run.sh separately starting backend and frontend

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-echo "build"
-# sudo docker-compose build --no-cache
-sudo docker-compose build
-echo "up"
-sudo docker-compose up
+# echo "build"
+# # sudo docker-compose build --no-cache
+# sudo docker-compose build
+# echo "up"
+# sudo docker-compose up
+docker-compose -f ./backend/docker-compose.yml up --build -d
+docker build -t yeungzed/weshop_frontend:latest --no-cache ./frontend
+# docker run -d -p 8080:80 -e PORT=80 -e BACKEND=nginx:80 --name frontend_1 --network=backend_default yeungzed/weshop_frontend:latest
+docker run -d -p 8080:80 -e PORT=80 -e BACKEND=api.weshop.zedyeung.com --name frontend_1 --network=backend_default yeungzed/weshop_frontend:latest
